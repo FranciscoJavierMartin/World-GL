93dda68e2fb0b3adef101d7411839460efaceb1f
francisco <francisco@localhost.localdomain>
2017-08-08 18:53:19 +0200

    3D Cube added

--- main.js
+++ main.js
@@ -2,20 +2,32 @@
 
 	//Definimos la escena
 	let scene=new THREE.Scene();
-
 	//Definimos la camara
 	const aspectRatio=window.innerWidth/window.innerHeight;
-
 	let camera =new THREE.PerspectiveCamera(75,aspectRatio,0.1,100);
-
 	//Definimos el render
 	let renderer=new THREE.WebGLRenderer();
-
 	renderer.setSize(window.innerWidth,window.innerHeight);
-
 	document.body.appendChild(renderer.domElement);
 
-	requestAnimationFrame(loop)
+	camera.position.z=5;
+	camera.position.y=2;
+
+	let geometry=new THREE.BoxGeometry(1,1,1);
+
+	let groundMaterial=new THREE.MeshPhongMaterial({
+		color: 0xffffff
+	})
+
+	let mesh=new THREE.Mesh(geometry,groundMaterial);
+
+	let pointLight=new THREE.PointLight(0xdfebff);
+
+	pointLight.position.y=20;
+
+	scene.add(mesh);
+	scene.add(new THREE.AmbientLight(0x404040))
+	scene.add(pointLight);
 
 	function loop(){
 		requestAnimationFrame(loop)
