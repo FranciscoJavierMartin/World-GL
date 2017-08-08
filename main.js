abec338c21e2daf1c232f379b8692a250be1ea39
francisco <francisco@localhost.localdomain>
2017-08-08 18:27:59 +0200

    Camera, scene and render added

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 (function(){
 
-	
+	//Definimos la escena
+	let scene=new THREE.Scene();
 
+	//Definimos la camara
+	const aspectRatio=window.innerWidth/window.innerHeight;
+
+	let camera =new THREE.PerspectiveCamera(75,aspectRatio,0.1,100);
+
+	//Definimos el render
+	let renderer=new THREE.WebGLRenderer();
+
+	renderer.setSize(window.innerWidth,window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+	renderer.render(scene,camera);
 })();
